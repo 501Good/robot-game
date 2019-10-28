f76a379749b7c65aea87c5cc473b063023e009f3
Timo Scholz <tischolz@uni-bremen.de>
2019-10-28 12:13:08 +0200

    Char Controller 2d

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Bullet BulletPrefab;
+
+    private PlayerController2D playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Shooting");
+            ShootBullet();
+        }
+    }
+
+    void ShootBullet()
+    {
+        Bullet bullet = GameObject.Instantiate<Bullet>(BulletPrefab, null);
+        bullet.transform.position = this.transform.position;
+        bullet.SetForwardDir(playerController.GetFacingDirection());
+        
+        
+    }
+}
