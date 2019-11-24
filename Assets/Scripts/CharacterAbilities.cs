50e1e4a02b2d6ccbad6726459301320da4d61dbd
Kirill Milintsevich <milintsevich@gmail.com>
2019-11-25 01:51:29 +0200

    New character added; Transformation added

--- Assets/Scripts/CharacterAbilities.cs
+++ Assets/Scripts/CharacterAbilities.cs
@@ -5,54 +5,47 @@ using UnityEngine.UI;
 
 public class CharacterAbilities : MonoBehaviour
 {
-    public bool TypeOneAvailable = false;
-    public bool TypeTwoAvailable = false;
-    public bool TypeThreeAvailable = false;
+    public PlayerController2D BaseCharacterPrefab;
+    public PlayerController2D AdditionalCharacterPrefab;
 
-    public Button TypeOneButton;
-    public Button TypeTwoButton;
+    public Cinemachine.CinemachineVirtualCamera PlayerCamera;
 
-    public PlayerController2D PlayerPrefab;
+    public bool AllowTransformation;
 
-    private void Awake()
+    private Animator anim;
+
+    private void Start()
     {
+        anim = GetComponent<Animator>();
     }
 
-    public void SetType(int type)
+    public void ChangeType()
     {
-        if (type > 0 && type <= 3)
+        if (AdditionalCharacterPrefab.gameObject.activeSelf)
         {
-            if (type == 1)
-            {
-                TypeOneAvailable = true;
-            }
-            else if (type == 2)
-            {
-                TypeTwoAvailable = true;
-                TypeOneButton.gameObject.SetActive(true);
-                TypeTwoButton.gameObject.SetActive(true);
-            }
-            else
-            {
-                TypeThreeAvailable = true;
-            }
+            var currentPos = gameObject.transform.position;
+            BaseCharacterPrefab.gameObject.transform.position = currentPos;
+            AdditionalCharacterPrefab.gameObject.SetActive(false);
+            BaseCharacterPrefab.gameObject.SetActive(true);
+            PlayerCamera.Follow = BaseCharacterPrefab.gameObject.transform;
+            PlayerCamera.LookAt = BaseCharacterPrefab.gameObject.transform;
+        } else
+        {
+            var currentPos = gameObject.transform.position;
+            AdditionalCharacterPrefab.gameObject.transform.position = currentPos;
+            BaseCharacterPrefab.gameObject.SetActive(false);
+            AdditionalCharacterPrefab.gameObject.SetActive(true);
+            PlayerCamera.Follow = AdditionalCharacterPrefab.gameObject.transform;
+            PlayerCamera.LookAt = AdditionalCharacterPrefab.gameObject.transform;
         }
     }
 
-    public void ChangeType(int type)
+    public void Update()
     {
-        if (type == 1)
+        if (Input.GetKeyDown(KeyCode.F) && AllowTransformation)
         {
-            Debug.Log("1");
-            PlayerPrefab.jumpForce = 35.0f;
-            PlayerPrefab.amountOfJumps = 1;
-            PlayerPrefab.SetColor(new Color(255f / 255.0f, 255f / 255.0f, 255f / 255.0f, 1f));
-        } else if (type == 2)
-        {
-            Debug.Log("2");
-            PlayerPrefab.jumpForce = 40.0f;
-            PlayerPrefab.amountOfJumps = 2;
-            PlayerPrefab.SetColor(new Color(233f / 255.0f, 91f / 255.0f, 91f / 255.0f, 1f));
+            anim.SetTrigger("Transforming");
+            ChangeType();
         }
     }
 }
