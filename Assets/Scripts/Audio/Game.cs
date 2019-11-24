6e856b40e95ae3dc9196323dd97868d64f5151a5
Kirill Milintsevich <milintsevich@gmail.com>
2019-11-24 18:24:57 +0200

    Main menu added

---
+++ Assets/Scripts/Audio/Game.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    private AudioSourcePool audio;
+
+    void Start()
+    {
+        audio = FindObjectOfType<AudioSourcePool>();
+        audio.Play("Background");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
