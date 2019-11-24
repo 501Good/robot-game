dc887622bb83edcea4cd8984a105ef5a6a5b4b4e
Kirill Milintsevich <milintsevich@gmail.com>
2019-11-24 22:43:47 +0200

    Added lights

---
+++ Assets/Scripts/Sparkle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sparkle : MonoBehaviour
+{
+    public ParticleSystem[] Sparks;
+    
+    public void CreateSpark(int idx)
+    {
+        Sparks[idx].Play();
+    }
+}
