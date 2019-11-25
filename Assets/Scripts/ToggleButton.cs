cc1553f3b5add91d42cd3e9d5f2a30fdf0e45e4e
Kirill Milintsevich <milintsevich@gmail.com>
2019-11-25 12:03:18 +0200

    Toggle button

---
+++ Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ToggleButton : MonoBehaviour
+{
+    public GameObject Togglable;
+
+    private TextMeshPro tip;
+
+    private bool canToggle;
+
+    public void Start()
+    {
+        tip = GetComponent<TextMeshPro>();
+        canToggle = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            tip.enabled = true;
+            canToggle = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            tip.enabled = false;
+            canToggle = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canToggle)
+        {
+            Togglable.SetActive(!Togglable.activeSelf);
+            this.gameObject.SetActive(false);
+        }
+    }
+
+
+
+
+}
