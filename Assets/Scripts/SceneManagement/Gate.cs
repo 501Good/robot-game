206a55f0bc3603f56b12bdc10843cfac112e4e5f
Kirill Milintsevich <milintsevich@gmail.com>
2020-01-17 10:49:47 +0200

    Safe area finished; Robot step sounds; Gate system;

---
+++ Assets/Scripts/SceneManagement/Gate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public string gateName = "";
+    public string otherGate = "";
+    public string otherScene = "";
+
+    public bool isOccupied;
+
+    public void Awake()
+    {
+        this.gameObject.name = gateName;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Action postLoad = () =>
+        {
+            Debug.Log("Postload started!");
+            Debug.Log("Name: " + gateName);
+            Debug.Log("otherGate: " + otherGate);
+            Debug.Log("otherScene: " + otherScene);
+            if (string.IsNullOrEmpty(otherGate)) {
+                Debug.Log("No otherGate");
+                return; 
+            }
+            Debug.Log("Finding other gate");
+            Gate newGate = GameObject.Find(otherGate).GetComponent<Gate>();
+            Debug.Log("New gate: " + newGate);
+            newGate.SetOccupied();
+            other.transform.position = newGate.transform.position;
+            Debug.Log("Postload finished");
+        };
+        
+        StartCoroutine(SceneChanger.ChangeScene(otherScene, this.gameObject.scene.name, postLoad));
+    }
+
+    void SetOccupied()
+    {
+        isOccupied = true;
+    }
+}
---
+++ Assets/Scripts/SceneManagement/SceneChanger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static string[] sceneNames = new string[] { "Level1", "SafeArea", "Level-something" };
+
+    public static IEnumerator ChangeScene(string newSceneName, string oldSceneName, Action postLoad)
+    {
+        if (IsSceneNameValid(newSceneName, oldSceneName))
+        {
+            
+            AsyncOperation loading = SceneManager.LoadSceneAsync(newSceneName, LoadSceneMode.Additive);
+            yield return loading;
+        }
+
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(newSceneName));
+        
+        postLoad();
+        Debug.Log("New scene: " + newSceneName);
+        if (!string.IsNullOrEmpty(newSceneName))
+        {
+            
+            yield return new WaitForEndOfFrame();
+            SceneManager.UnloadSceneAsync(oldSceneName);
+        }
+    }
+
+    public static bool IsSceneNameValid(string newSceneName, string oldSceneName)
+    {
+        Debug.Log(sceneNames.Contains(newSceneName));
+        return sceneNames.Contains(newSceneName) && sceneNames.Contains(oldSceneName);
+    }
+}
